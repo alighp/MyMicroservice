7465e9a9d54a77beb6ff6c509b19df1de9b6e643
alighp <alighanaatpisheh@gmail.com>
2026-06-29 09:13:55 +0330

    initial commit

---
+++ src/01.Core/SampleApplication.Core.Domain/Person.cs
@@ -0,0 +1,39 @@
+﻿namespace SampleApplication.Core.Domain
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public List<PhoneNumber> PhoneNumbers { get; set; } = new();
+        private Person()
+        {
+        }
+        public Person(string firstName, string lastName, PhoneNumber phoneNumber)
+        {
+            if (firstName == null || string.IsNullOrWhiteSpace(firstName))
+            {
+                throw new ArgumentNullException("Invalid input for firstName");
+            }
+            if (lastName == null || string.IsNullOrWhiteSpace(lastName))
+            {
+                throw new ArgumentNullException("Invalid input for lastName");
+            }
+            if (phoneNumber == null || string.IsNullOrWhiteSpace(phoneNumber.Number))
+            {
+                throw new ArgumentNullException("Invalid input for phoneNumber");
+            }
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            PhoneNumbers.Add(phoneNumber);
+        }
+
+        public void AddPhoneNumber(PhoneNumber phoneNumber)
+        {
+            if (PhoneNumbers.Any(x => x.Number == phoneNumber.Number))
+                throw new InvalidDataException("PhoneNumber is duplicate");
+            PhoneNumbers.Add(phoneNumber);
+        }
+    }
+}
---
+++ src/01.Core/SampleApplication.Core.Domain/PhoneNumber.cs
@@ -0,0 +1,19 @@
+﻿namespace SampleApplication.Core.Domain
+{
+    public class PhoneNumber 
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public PhoneNumber()
+        {
+
+        }
+        public PhoneNumber(string phoneNumber)
+        {
+            if (string.IsNullOrWhiteSpace(phoneNumber))
+            {
+                throw new ArgumentNullException("Invalid input for phoneNumber");
+            }
+        }
+    }
+}
